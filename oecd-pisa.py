d866ae30da5e258e94a934b28b35cfa947bc987f
Zeynep Elabiad <zeynepelabiad@gmail.com>
2018-03-11 18:45:26 -0400

    Data Ingestion

--- oecd-pisa.py
+++ oecd-pisa.py
@@ -33,12 +33,11 @@ f.close()
 ###################################################
 
 
-colnames=['Country Code','Indicator','Year','Value'] 
-
-Maths_Performance_2015=pd.read_csv('DATA/MathsPerformance/PISA_Math_all.csv', names=colnames,skiprows=[0], header=None,dtype=object,usecols=[0,1,5,6])
+Maths_Performance_2015=pd.read_csv('DATA/MathsPerformance/PISA_Maths_all.csv',dtype=object,usecols=[0,1,2,5,6])
+Maths_Performance_2015.rename(columns={'LOCATION': 'Country_code','TIME':'Year'}, inplace=True)
 print(Maths_Performance_2015.head())
 
-Maths_Performance_2015=Maths_Performance_2015[(Maths_Performance_2015['Year']>='2015')]
+Maths_Performance_2015=Maths_Performance_2015[(Maths_Performance_2015['Year']>='2015') & (Maths_Performance_2015['SUBJECT']=='TOT')]
 
 Maths_Performance_2015.rename(columns={'Country Code': 'Country_code'}, inplace=True)
 
@@ -46,3 +45,38 @@ with open('3-Maths_Performance_2015.csv', 'w') as f:
     
     Maths_Performance_2015.to_csv(f, header=True,index=0)
 f.close()
+
+
+
+Reading_Performance_2015=pd.read_csv('DATA/ReadingPerformance/PISA_Reading_all.csv',dtype=object,usecols=[0,1,2,5,6])
+Reading_Performance_2015.rename(columns={'LOCATION': 'Country_code','TIME':'Year'}, inplace=True)
+print(Maths_Performance_2015.head())
+
+Reading_Performance_2015=Reading_Performance_2015[(Reading_Performance_2015['Year']>='2015') & (Reading_Performance_2015['SUBJECT']=='TOT')]
+
+Reading_Performance_2015.rename(columns={'Country Code': 'Country_code'}, inplace=True)
+
+with open('4-Reading_Performance_2015.csv', 'w') as f:
+    
+    Reading_Performance_2015.to_csv(f, header=True,index=0)
+f.close()
+
+
+
+Science_Performance_2015=pd.read_csv('DATA/SciencePerformance/PISA_Science_all.csv',dtype=object,usecols=[0,1,2,5,6])
+Science_Performance_2015.rename(columns={'LOCATION': 'Country_code','TIME':'Year'}, inplace=True)
+print(Science_Performance_2015.head())
+
+Science_Performance_2015=Science_Performance_2015[(Science_Performance_2015['Year']>='2015') & (Science_Performance_2015['SUBJECT']=='TOT')]
+
+Science_Performance_2015.rename(columns={'Country Code': 'Country_code'}, inplace=True)
+
+with open('5-Science_Performance_2015.csv', 'w') as f:
+    
+    Science_Performance_2015.to_csv(f, header=True,index=0)
+f.close()
+
+Pisa_df_2015=[Maths_Performance_2015,Reading_Performance_2015,Science_Performance_2015]
+Pisa_df_2015= reduce(lambda  left,right: pd.merge(left,right,on='Country_code', how='left'), Pisa_df_2015)
+with open('6-Pisa_df_2015.csv', 'w') as f:
+    Pisa_df_2015.to_csv(f, header=True,index=0)
