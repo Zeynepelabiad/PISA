8ec58d18b18806f3df723953d4f9a63dba1a5414
Zeynep Elabiad <zeynepelabiad@gmail.com>
2018-03-13 23:20:19 -0400

    Data Ingestion & Wrangling & Visualization

--- oecd-pisa.py
+++ oecd-pisa.py
@@ -23,6 +23,8 @@ with open('1-GDP_Per_Capita.csv', 'w') as f:
     GDP_Per_Capita.to_csv(f, header=True,index=0,float_format='%.3f')
 f.close()
 
+
+
 ################################################
 
 #Population_by_country=pd.read_csv('DATA/Population-by-country-all.csv',dtype=object,skiprows=[0,2,3],usecols=[1,*range(50,60)])
@@ -95,12 +97,76 @@ f.close()
 
 Pisa_df_2015=[Math_Performance_2015,Reading_Performance_2015,Science_Performance_2015,GDP_Per_Capita,Population_by_country,Life_expectancy]
 Pisa_df_2015= reduce(lambda  left,right: pd.merge(left,right,on='Country_code', how='left'), Pisa_df_2015)
+Pisa_df_2015=Pisa_df_2015.dropna()
 
 with open('7-Pisa_df_2015.csv', 'w') as f:
     Pisa_df_2015.to_csv(f, header=True,index=0,columns=['Country_code','MATH2015','READ2015','SCIENCE2015','GDP_2015','Population_2015','Life_E_2015'])
 
 f.close()
 
+
+with sns.axes_style('white'):
+    g = sns.jointplot("SCIENCE2015", "Life_E_2015", Pisa_df_2015, kind='hex')
+    g.ax_joint.plot(np.linspace(0, 500),
+                    np.linspace(50, 100), ':k')
+    plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+ 
+# create data
+x = Pisa_df_2015['Country_code']
+y = Pisa_df_2015['MATH2015']
+z = Pisa_df_2015['Life_E_2015']
+ 
+# use the scatter function
+plt.scatter(x, y, s=z*5, alpha=0.5)
+plt.show()
+
+
+plt.style.use('ggplot')
+Pisa_df_2015.plot(kind='barh', y="GDP_2015", x="Country_code")
+plt.show()
+
+plt.style.use('ggplot')
+Pisa_df_2015.plot(kind='barh', y="Population_2015", x="Country_code")
+plt.show()
+
+plt.style.use('ggplot')
+Pisa_df_2015.plot(kind='barh', y="Life_E_2015", x="Country_code")
+plt.show()
+
+plt.style.use('ggplot')
+Pisa_df_2015.plot(kind='barh', y="MATH2015", x="Country_code")
+plt.show()
+
+plt.style.use('ggplot')
+Pisa_df_2015.plot(kind='barh', y="READ2015", x="Country_code")
+plt.show()
+
+plt.style.use('ggplot')
+Pisa_df_2015.plot(kind='barh', y="SCIENCE2015", x="Country_code")
+plt.show()
+
+
+
+with sns.axes_style('white'):
+    sns.jointplot("READ2015", "Life_E_2015", data=Pisa_df_2015, kind='hex')
+    plt.show()
+
+
+
+sns.jointplot("GDP_2015", "Life_E_2015", data=Pisa_df_2015, kind='reg');
+plt.show()
+
+sns.jointplot("MATH2015", "GDP_2015", data=Pisa_df_2015, kind='reg');
+plt.show()
+
+sns.jointplot("MATH2015", "Life_E_2015", data=Pisa_df_2015, kind='reg');
+plt.show()
+
+sns.jointplot("SCIENCE2015", "Life_E_2015", data=Pisa_df_2015, kind='reg');
+plt.show()
 
 Pisa_df_2015.plot(kind='scatter', x='GDP_2015', y='Life_E_2015', c='Population_2015',figsize=[20,10])
 plt.show()
@@ -110,3 +176,8 @@ from pandas.tools.plotting import scatter_matrix
 areas = Pisa_df_2015[['Country_code','MATH2015','READ2015','SCIENCE2015','GDP_2015','Population_2015','Life_E_2015']]
 scatter_matrix(areas, alpha=0.2, figsize=(18,18), diagonal='kde')
 plt.show()
+
+
+g = sns.pairplot(data=Pisa_df_2015[['Country_code','MATH2015','READ2015','SCIENCE2015','GDP_2015','Population_2015','Life_E_2015']], hue='Country_code', dropna=True)
+plt.show()
+"""
